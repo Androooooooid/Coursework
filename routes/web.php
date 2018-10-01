47de56a9bfa704ba34865a212a1a6340f4dceaa9
Андрей <32129336+Androooooooid@users.noreply.github.com>
2018-10-01 23:08:01 +0400

    create web routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'web'],function() {
+	
+	Route::match(['get','post'],'/',['uses'=>'IndexConteroller@execute','as'=>'home']);
+	Route::get('/page/{alias}',['uses'=>'PageController@execute','as'=>'page']);
+	
+	Route::auth();
+	
+});
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function(); {
+	
+	Route::get('/',function() {
+		
+		
+	});
+	
+Route::group(['prefix'=>'pages'], function() {
+	
+	Route::get('/',['uses'=>'PageController@execute','as'=>'pages']);
+	
+	Route::match(['get','post'],'/add',['uses'=>'PageController@execute','as'=>'pagesAdd']);
+	
+	Route::match(['get','post','delete'],'/edit/',['uses'=>'PagesController@execute','as'=>'pagesEdit']);
+	
+});
+
+Route::group(['prefix'=>'sevices'], function() {
+	
+	Route::get('/',['uses'=>'ServiceController@execute','as'=>'sevices']);
+	
+	Route::match(['get','post'],'/add',['uses'=>'ServiceController@execute','as'=>'serviceAdd']);
+	
+	Route::match(['get','post','delete'],'/edit/',['uses'=>'ServiceController@execute','as'=>'serviceEdit']);
+	
 });
