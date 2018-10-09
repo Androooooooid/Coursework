c9f02197b59f5a28e49c337aa6b7c725f9cdf5f4
Андрей <32129336+Androooooooid@users.noreply.github.com>
2018-10-10 00:31:10 +0400

    I divided the pattern into logical parts and checked the data output from the table.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Service;
+use App\People;
+
+
 class IndexController extends Controller
 {
     //
 	public function execute(Request $request) {
-		return view('frontend.site');
+		
+		$pages = Page::all();
+		$service = Page::all();
+		$people = People::all();
+		
+		return view('backend.index');
 	}
 }
---
+++ resources/views/backend/index.blade.php
@@ -0,0 +1,9 @@
+@extends ('frontend.site')
+
+@section ('header')
+	@include('frontend.header')
+@endsection
+
+@section('content')
+	@include('frontend.content')
+@endsection
